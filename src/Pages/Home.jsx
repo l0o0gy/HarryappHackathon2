d4bd3f63bc30b96fdbd82c2c9f1fe44bba4d85db
l0o0gy <minamahone9620@gmail.com>
2024-07-08 19:36:16 +0300

    add change to home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-// import MiniDrawer from '../Components/drawer'
-import ChartsOverviewDemo from '../Components/BarChart'
 import Navbar from '../Components/Navbar'
-import Detailsbox from '../Components/Detailsbox'
+import Detailsbox from '../Components/TemperatureCard'
+import HumidityCard from '../Components/HumidityCard'
+import  GasSensor from '../Components/GassSensor'
+import Concentration from '../Components/Concentration';
+import Heat from '../Components/Heat'
+import Table  from '../Pages/Table'
+// import WebSocketComponent from '../Components/WebSocket'
+
 function Home() {
   return (
-    <div>
+    <div className='home'>
         <Navbar/>
-        <Detailsbox/>
-      {/* <MiniDrawer/> */}
-      <ChartsOverviewDemo/>
+        {/* <MainCard/> */}
+        {/* <WebSocketComponent/> */}
+        <Heat/>
+        <div style={{display:"flex" , justifyContent:'space-evenly'}}>
+          <Detailsbox/>
+          <HumidityCard/>
+          <GasSensor/>
+          <Concentration/>
+        </div> 
+        <div style={{display:"flex" , justifyContent:'center'}}>
+         <Table/>
+         </div>
     </div>
   )
 }
