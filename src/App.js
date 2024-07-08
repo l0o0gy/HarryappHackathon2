65caae9f714126e44c925c8bbfaf84a02363873f
l0o0gy <minamahone9620@gmail.com>
2024-07-08 19:28:50 +0300

    add history route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import TempHistory from './Pages/TempHistory';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
       <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/temphisto' element={<TempHistory/>}/>
       </Routes>
       </BrowserRouter>
     </div>
